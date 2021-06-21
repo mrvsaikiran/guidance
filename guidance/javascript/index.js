cabe1bcd044fbd09fa412835c27e6abe1275ec86
saikiran <saikiran200203@gmail.com>
2021-06-21 12:36:52 +0530

    initial commit

---
+++ guidance/javascript/index.js
@@ -0,0 +1,7 @@
+console.log('aceesd');
+// alert('js linked');
+var a=document.getElementById('y-item[0]');
+a.style.order='1';
+var b=document.getElementById('y-item[1]');
+b.style.order='2';
+console.log(a.style.order)
